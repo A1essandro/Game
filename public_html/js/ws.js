56a9133a7b76ca0616c9e198d315865f6fe2dd91
A1essandro <alexander.yermakov.1990@gmail.com>
2015-05-27 00:30:45 +0400

    Изменена формула набора скорости

--- public_html/js/ws.js
+++ public_html/js/ws.js
@@ -8,7 +8,7 @@ Number.prototype.toRad = function () {
 
 Player = function () {
     this.speed = 0;
-    this.acceleration = 0.1;
+    this.acceleration = 0.01;
     this.rotation = 0;
     this.rotationSpeed = 2;
     this.position = {
@@ -16,8 +16,6 @@ Player = function () {
         y: 0
     };
     this.update = function () {
-        if(this.speed > 0)
-            this.speed *= 0.985;
         this.position.x += Math.sin(this.rotation.toRad()) * this.speed;
         this.position.y += -Math.cos(this.rotation.toRad()) * this.speed;
         $('#player').css({top: this.position.y});
@@ -28,13 +26,14 @@ Player = function () {
             'transform': 'rotate(' + this.rotation + 'deg)'});
     };
     this.rotate = function (deg) {
-        this.speed *= 0.985;
+        this.speed *= 0.99;
         speedFactor = this.speed / 2.5 + 1
         console.log(speedFactor);
         this.rotation += this.rotationSpeed * deg / speedFactor;
     };
     this.speedUp = function () {
-        player.speed += player.acceleration;
+        if(this.speed < 1000 * this.acceleration)
+            player.speed += player.acceleration;
     };
 };
 
@@ -43,12 +42,13 @@ var player = new Player();
 function main() {
     if (input.isDown('UP'))
         player.speedUp();
+    else if(player.speed < 0.075)
+        player.speed = 0;
+    else
+        player.speed *= 0.995;
 
     if (input.isDown('DOWN'))
-        player.speed *= 0.93;
-
-    if (player.speed < 0.075)
-        player.speed = 0;
+        player.speed *= 0.98;
 
     if (input.isDown('LEFT'))
         player.rotate(-1);
